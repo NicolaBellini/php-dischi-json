0420a1a353eac1af7497cb7f6c72efac746e55da
NicolaBellini <bellininicola4366@gmail.com>
2024-04-17 14:05:43 +0200

    [add] getapi function, print on page of bootstrap cards, and server php call and get content rom json

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const { createApp } = Vue;
 
-const app = Vue.createApp({
+const app = createApp({
   data() {
     return {
       title: "loco",
+      discList: [],
+      apiUrl: "server.php",
     };
+  },
+  methods: {
+    getApi(apiUrl) {
+      axios.get(apiUrl).then((res) => {
+        this.discList = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.getApi(this.apiUrl);
   },
 }).mount("#app");
--- server.php
+++ server.php
@@ -1,3 +1,16 @@
 <?php 
 
+$json_String = file_get_contents('disc-list.json');
+
+$discsList = json_decode($json_String);
+
+// Qui ci va la logica 
+
+
+header('Content-Type: application/json');
+
+// stampo il json in pagina
+echo json_encode($discsList);
+
+
 ?>
