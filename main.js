49f419400d411f3f5950b8e2d9f50412ab401e85
NicolaBellini <bellininicola4366@gmail.com>
2024-04-18 14:43:53 +0200

    [fix] function senddata, wrong key in newAuthor, now works fine

--- main.js
+++ main.js
@@ -27,11 +27,30 @@ createApp({
       // prima trasformo i dati
       const data = new FormData();
       data.append("newTitle", this.newDisc.title);
-      data.append("genre", this.newDisc.author);
+      data.append("newAuthor", this.newDisc.author);
       data.append("newYear", this.newDisc.year);
       data.append("newPoster", this.newDisc.poster);
       data.append("newGenre", this.newDisc.genre);
       // ora faccio la chiamata axios in .post
+      axios.post(this.apiUrl, data).then((res) => {
+        this.discList = res.data;
+        console.log(res.data);
+        // una volta inviato i dati e aver riscritto la lista devo resettare i campi di input se no continua a stamparmi card
+      });
+      this.newDisc.title = "";
+      this.newDisc.author = "";
+      this.newDisc.year = "";
+      this.newDisc.poster = "";
+      this.newDisc.genre = "";
+    },
+
+    // funzione per la rimozione di un disco
+    deleteDisc(index) {
+      // passo l' indice dell' elemento da eliminare ottenuto con il ciclo v-for
+      const data = new FormData();
+      data.append("indexToDelete", index);
+
+      // invio i dati al server e riscrivo la lista con i dati aggiornati
       axios.post(this.apiUrl, data).then((res) => {
         this.discList = res.data;
         console.log(res.data);
--- server.php
+++ server.php
@@ -5,19 +5,25 @@ $json_String = file_get_contents('disc-list.json');
 $discsList = json_decode($json_String, true);
 
 // Qui ci va la logica 
-
-// creo il nuovo oggetto e lo aggiungo al json
-if(isset($_POST["newTitle"])){
-  $newDisc=[
-    'title'=>$_POST['newTitle'],
-    'author'=>$_POST['newAuthor'],
-    'year'=>$_POST['newYear'],
-    'poster'=>$_POST['newPoster'],
-    'genre'=>$_POST['newGenre'],    
+  
+// Aggiungere un nuovo disco
+if (isset($_POST["newTitle"])) {
+  $newDisc = [
+    'title' => $_POST['newTitle'],
+    'author' => $_POST['newAuthor'],
+    'year' => $_POST['newYear'],
+    'poster' => $_POST['newPoster'],
+    'genre' => $_POST['newGenre'],    
   ];
-  $discsList[]=$newDisc;
+  $discsList[] = $newDisc;
   file_put_contents('disc-list.json', json_encode($discsList));
-};
+}
+
+// Eliminare un disco
+if (isset($_POST['indexToDelete'])) {
+  array_splice($discsList, $_POST['indexToDelete'], 1);
+  file_put_contents('disc-list.json', json_encode($discsList));
+}
 
 
 
