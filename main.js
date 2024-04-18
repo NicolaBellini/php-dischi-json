b95a910883c8ed70d72d7323eaa6dd364f72ec95
NicolaBellini <bellininicola4366@gmail.com>
2024-04-18 13:55:18 +0200

    [add] php logic and main.js function to push into the json file the newDisc

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ createApp({
       discList: [],
       apiUrl: "server.php",
       newDisc: {
-        text: "",
-        year: "",
+        title: "",
+        author: "",
+        year: null,
+        poster: "",
+        genre: "",
       },
     };
   },
@@ -16,6 +19,22 @@ createApp({
     getApi(apiUrl) {
       axios.get(apiUrl).then((res) => {
         this.discList = res.data;
+      });
+    },
+
+    // qui faccio la funzione per inviare i dati a php sotto forma di formdata
+    sendData() {
+      // prima trasformo i dati
+      const data = new FormData();
+      data.append("newTitle", this.newDisc.title);
+      data.append("genre", this.newDisc.author);
+      data.append("newYear", this.newDisc.year);
+      data.append("newPoster", this.newDisc.poster);
+      data.append("newGenre", this.newDisc.genre);
+      // ora faccio la chiamata axios in .post
+      axios.post(this.apiUrl, data).then((res) => {
+        this.discList = res.data;
+        console.log(res.data);
       });
     },
   },
--- server.php
+++ server.php
@@ -2,11 +2,22 @@
 
 $json_String = file_get_contents('disc-list.json');
 
-$discsList = json_decode($json_String);
+$discsList = json_decode($json_String, true);
 
 // Qui ci va la logica 
 
-
+// creo il nuovo oggetto e lo aggiungo al json
+if(isset($_POST["newTitle"])){
+  $newDisc=[
+    'title'=>$_POST['newTitle'],
+    'author'=>$_POST['newAuthor'],
+    'year'=>$_POST['newYear'],
+    'poster'=>$_POST['newPoster'],
+    'genre'=>$_POST['newGenre'],    
+  ];
+  $discsList[]=$newDisc;
+  file_put_contents('disc-list.json', json_encode($discsList));
+};
 
 
 
